1a2b08676194b9b71cf7383a31ad0f3ebd10a2d2
Planxty-Dan <dan.newport@detroitlabs.com>
2019-02-01 10:08:09 -0500

    adding todolist item fields

--- src/TodoListItem.java
+++ src/TodoListItem.java
@@ -1,4 +1,26 @@
 public class TodoListItem {
 
+    private String title;
     private String description;
+
+    public void printDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Description: " + description);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
