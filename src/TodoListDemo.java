37867aab4128474da187421434546a742a772642
Planxty-Dan <dan.newport@detroitlabs.com>
2019-02-01 10:21:16 -0500

    creating menu, user options, switch statement for user input

--- src/TodoListDemo.java
+++ src/TodoListDemo.java
@@ -1,6 +1,43 @@
+import java.util.Scanner;
+
 public class TodoListDemo {
 
-    public static void main(String[] args) {
+    private static Scanner scanner = new Scanner(System.in);
 
+
+    public static void main(String[] args) {
+        System.out.println("Choose an option: \n" +
+                "1. Add an item \n" +
+                "2. Display all items \n" +
+                "3. Display todos not started \n" +
+                "4. Display in progress items \n" +
+                "5. Display done items \n" +
+                "6. Edit an item \n" +
+                "7. Delete an item \n" +
+                "8. Exit");
+
+        int userInput = scanner.nextInt();
+
+        switch (userInput) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            default:
+                break;
+
+        }
     }
 }
